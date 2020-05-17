765b504cf1302b49a3c3f16ea9acb499f87e4216
André Santos <andre@dexdev.com.br>
2020-05-17 16:01:07 -0300

    feat(route): add users API crud endpoints

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::get('/users', 'Api\UsersListController');
+Route::post('/users', 'Api\UsersCreateController');
+Route::put('/users/{id}', 'Api\UsersUpdateController');
+Route::delete('/users/{id}', 'Api\UsersDeleteController');
