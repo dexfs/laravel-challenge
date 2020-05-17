192df8aec00fd93c7be68c00fa02c73f4d327ed3
André Santos <andre@dexdev.com.br>
2020-05-17 16:13:43 -0300

    feat(test): users test
    testing crud

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUsersList()
+    {
+        factory(\App\User::class, 10)->create();
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('GET', '/api/users');
+        $data = $response->decodeResponseJson();
+        $response->assertSuccessful();
+        self::assertCount(10, $data['data']);
+    }
+
+    public function testUserCreate()
+    {
+        $user = factory(\App\User::class)->make();
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('POST', '/api/users', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => Hash::make('123456789')
+        ] );
+        $response->assertCreated();
+    }
+
+    public function testUserCreateFail()
+    {
+        $user = factory(\App\User::class)->make();
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('POST', '/api/users', [
+            'name' => $user->name,
+            'email' => $user->email,
+        ] );
+        $response->assertStatus(422);
+    }
+
+    public function testUserUpdate()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('PUT', "/api/users/$user->id", [
+            'name' => 'Updated Name',
+        ] );
+
+        $response->assertStatus(200);
+    }
+    public function testUserUpdateWithNoValidId()
+    {
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('PUT', "/api/users/0");
+
+        $response->assertStatus(404);
+    }
+
+    public function testUserDelete()
+    {
+        $user = factory(\App\User::class)->create();
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('DELETE', "/api/users/$user->id");
+
+        $response->assertStatus(200);
+    }
+
+    public function testUserDeleteFail()
+    {
+        $response = $this->withHeaders(
+            ['Accept' => 'application/json']
+        )->json('DELETE', "/api/users/0");
+
+        $response->assertStatus(404);
+    }
+}
